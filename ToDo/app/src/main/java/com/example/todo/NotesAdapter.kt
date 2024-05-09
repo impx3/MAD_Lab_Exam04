da18121f9253419ca13e07f124269835850aa7cc
J. A. P. P. Perera <jappp020618@gmail.com>
2024-05-09 22:48:30 +0530

    Updated NoteAdapter

--- ToDo/app/src/main/java/com/example/todo/NotesAdapter.kt
+++ ToDo/app/src/main/java/com/example/todo/NotesAdapter.kt
@@ -1,6 +1,36 @@
 package com.example.todo
 
-class NotesAdapter{
+import android.content.Context
+import android.view.View
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.Textview
 
-    
+class NotesAdapter (private var notes: List<Note>, context:Context):RecyclerView.Adapter<NotesAdapter.NoteViewHolder>{
+
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val TV_title: Textview = itemView.findViewById(R.id.TV_title)
+        val TV_content : Textview = itemView.findViewById(R.id.TV_content)
+
+    }
+
+    override fun onCreateViewHolder (parent: ViewGroup, viewType: Int): NoteViewHolder{
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false)
+        return NoteViewHolder (view)
+    }
+
+    override fun getItemcount(): Int = notes.size
+
+    override fun onBindViewHolder (holder: NoteViewHolder, position: Int) {
+        val note = notes(position)
+        holder.TV_title.text = note.title
+        holder.TV_content.text = note.content
+    }
+
+    fun refreshData(newNotes: List<Note>){
+        notes = newNotes
+        notifyDataSetChanged()
+    }
+
 }
