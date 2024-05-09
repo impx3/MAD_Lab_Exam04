a5fa62937e297e9fff3fe7e0102e1c98242273b9
J. A. P. P. Perera <jappp020618@gmail.com>
2024-05-09 18:59:48 +0530

    Added and updated kotlin files

--- ToDo/app/src/main/java/com/example/todo/AddNoteActivity.kt
+++ ToDo/app/src/main/java/com/example/todo/AddNoteActivity.kt
@@ -5,12 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.todo.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db: NotesDatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        setContentView(R.layout.activity_add_note)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
        
+       db = NotesDatabaseHelper(this)
+
+       binding.saveButton.setOnClickListener {
+            val title = binding.ET_note_heading.text.toString()
+            val content = binding.ET_note_content.text.toString()
+            val note = Note (0, title, content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show()
+       }
     }
 }
