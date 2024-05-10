06298ccf812c58424888105996fb6c7adb24007c
J. A. P. P. Perera <jappp020618@gmail.com>
2024-05-10 12:21:38 +0530

    Created UpdateActivity

---
+++ ToDo/app/src/main/java/com/example/todo/UpdateActivity.kt
@@ -0,0 +1,17 @@
+package com.example.todo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.todo.databinding.ActivityMainBinding
+
+class UpdateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update)
+    }    
+
+}
