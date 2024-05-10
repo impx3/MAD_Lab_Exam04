0de76b9d14a1fc447ce0314cb59c77cf218376d7
J. A. P. P. Perera <jappp020618@gmail.com>
2024-05-10 12:59:09 +0530

    Edited UpdateActivity

---
+++ ToDo/app/src/main/java/com/example/todo/UpdateNoteActivity.kt
@@ -0,0 +1,43 @@
+package com.example.todo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.todo.databinding.ActivityUpdateBinding
+
+class UpdateNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUpdateBinding
+    private lateinit var db: NotesDatabaseHelper
+    private var noteId: Int = -1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUpdateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+       
+        db = NotesDatabaseHelper(this)
+
+        noteId = intent.getIntExtra("note_id", -1)
+        if (noteId == -1){
+            finish()
+            return
+        }
+
+        val note = db.getNoteByID(noteId)
+        binding.ET_update_note_heading.setText(note.title)
+        binding.ET_update_note_content.setText(note.content)
+
+        binding.updateSavebutton.setOnClickListener{
+            val newTitle = binding.ET_update_note_content.text.toString()
+            val newContent = binding.T_update_note_content.text.toString()
+            val updatedNote = Note (noteId, newTitle, newContent)
+            db.updateNote(updatedNote)
+            finish()
+            Toast.makeText(this, "Changes Saved",Toast.LENGTH_SHORT).show()
+        }
+    }    
+
+}
